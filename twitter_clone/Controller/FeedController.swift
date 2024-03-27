63b959a6a072cde59b25e0e13d00eb3bc610890d
jungseok-corine <nonehour@naver.com>
2024-03-27 13:58:58 +0900

    sotryboard delete

---
+++ twitter_clone/Controller/FeedController.swift
@@ -0,0 +1,24 @@
+//
+//  FeedController.swift
+//  twitter_clone
+//
+//  Created by 오정석 on 27/3/2024.
+//
+
+import UIKit
+
+class FeedController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+  
+    // MARK: - Helpers
+}
---
+++ twitter_clone/Controller/MainTabController.swift
@@ -0,0 +1,31 @@
+//
+//  MainTabController.swift
+//  twitter_clone
+//
+//  Created by 오정석 on 27/3/2024.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+  
+    // MARK: - Helpers
+    
+    
+    func configureViewControllers() {
+        
+    }
+
+
+}
