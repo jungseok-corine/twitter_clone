2ee094c61878d4db6d7b61a2164d95a75dc0ce8f
jungseok-corine <nonehour@naver.com>
2024-03-28 13:08:33 +0900

    navibar add, titile add

--- twitter_clone/Controller/MainTabController.swift
+++ twitter_clone/Controller/MainTabController.swift
@@ -16,8 +16,7 @@ class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .brown
         configureViewControllers()
     }
   
@@ -27,20 +26,26 @@ class MainTabController: UITabBarController {
     func configureViewControllers() {
         
         let feed = FeedController()
-        feed.tabBarItem.image = UIImage(named: "home_unselected")
+        let nav1 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: feed)
         
         let explore = ExploreController()
-        explore.tabBarItem.image = UIImage(named: "search_unselected")
+        let nav2 = templateNavigationController(image: UIImage(named: "search_unselected"), rootViewController: explore)
         
         let notifications = NotificationController()
-        notifications.tabBarItem.image = UIImage(named: "like_unselected")
+        let nav3 = templateNavigationController(image: UIImage(named: "like_unselected"), rootViewController: notifications)
         
         let conversations = ConversationsController()
-        conversations.tabBarItem.image = UIImage(named: "like_unselected")
+        let nav4 = templateNavigationController(image: UIImage(named: "ic_mail_outline_white_2x-1"), rootViewController: conversations)
         
         
-        viewControllers = [feed, explore, notifications, conversations]
+        viewControllers = [nav1, nav2, nav3, nav4]
     }
 
-
+    func templateNavigationController(image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = image
+        nav.navigationBar.barTintColor = .white
+        return nav
+    }
+    
 }
