564737afa03fd8c93112e6cfe28e1e8fbc251155
jungseok-corine <nonehour@naver.com>
2024-04-13 16:45:29 +0900

    login check and UI setup

--- twitter_clone/Controller/MainTabController.swift
+++ twitter_clone/Controller/MainTabController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MainTabController: UITabBarController {
 
@@ -24,9 +25,32 @@ class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
-        configureViewControllers()
-        configureUI()
+//        logUserOut()
+        authenticateUserAndConfigureUI()
+    }
+    
+    // MARK: - API
+    
+    // 로그인 했는지 안했는지 확인
+    func authenticateUserAndConfigureUI() {
+        if Auth.auth().currentUser == nil {
+            DispatchQueue.main.async {
+                let nav = UINavigationController(rootViewController: LoginController())
+                nav.modalPresentationStyle = .fullScreen
+                self.present(nav, animated: true, completion: nil)
+            }
+        } else {
+            configureViewControllers()
+            configureUI()
+        }
+    }
+    
+    func logUserOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch let error {
+            print("DEBUG: Failed to sign out with error \(error.localizedDescription)")
+        }
     }
   
     // MARK: - Selectors
