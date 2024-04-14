716807ae3bf9bb003477ec35a01bf0d4ec1c0d16
jungseok-corine <nonehour@naver.com>
2024-04-14 17:10:22 +0900

    SDWebimage install

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-platform :ios, '13.0'
+platform :ios, '15.0'
 
 target 'twitter_clone' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -11,4 +11,5 @@ target 'twitter_clone' do
 	pod 'Firebase/Database'
 	pod 'Firebase/Storage'
 	pod 'Firebase/Auth'
+	pod 'SDWebImage', '~> 5.0'
 end
--- twitter_clone/Controller/FeedController.swift
+++ twitter_clone/Controller/FeedController.swift
@@ -22,8 +22,16 @@ class FeedController: UIViewController {
     // MARK: - Helpers
     func configureUI() {
         view.backgroundColor = .white
+        
         let imageView = UIImageView(image: UIImage(named: "twitter_logo_blue"))
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
+        
+        let profileImageView = UIImageView()
+        profileImageView.backgroundColor = .twitterBlue
+        profileImageView.setDimensions(width: 32, height: 32)
+        profileImageView.layer.cornerRadius = 32 / 2
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: profileImageView)
     }
 }
