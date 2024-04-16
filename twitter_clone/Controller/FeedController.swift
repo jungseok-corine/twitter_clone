71bf92dcc20187d709704a054fcd3e7f8b382edd
jungseok-corine <nonehour@naver.com>
2024-04-16 18:24:12 +0900

    load user profile image

--- twitter_clone/Controller/FeedController.swift
+++ twitter_clone/Controller/FeedController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class FeedController: UIViewController {
     
@@ -13,7 +14,7 @@ class FeedController: UIViewController {
     
     var user: User? {
         didSet{
-            print("DEBUG: Did set user in feed controller..")
+            configureLeftBarButton()
         }
     }
     
@@ -33,10 +34,18 @@ class FeedController: UIViewController {
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
         
+        
+    }
+    
+    func configureLeftBarButton() {
+        guard let user = user else { return }
+        
         let profileImageView = UIImageView()
-        profileImageView.backgroundColor = .twitterBlue
         profileImageView.setDimensions(width: 32, height: 32)
         profileImageView.layer.cornerRadius = 32 / 2
+        profileImageView.layer.masksToBounds = true
+        
+        profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: profileImageView)
     }
