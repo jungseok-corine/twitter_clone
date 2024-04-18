85338f4a0b00030d2a50acad2116086fe37ec9bb
jungseok-corine <nonehour@naver.com>
2024-04-18 15:30:36 +0900

    populate feed with tweets

--- twitter_clone/Controller/FeedController.swift
+++ twitter_clone/Controller/FeedController.swift
@@ -18,6 +18,10 @@ class FeedController: UICollectionViewController {
         didSet{ configureLeftBarButton() }
     }
     
+    private var tweets = [Tweet]() {
+        didSet { collectionView.reloadData() }
+    }
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -30,7 +34,7 @@ class FeedController: UICollectionViewController {
     
     func fetchTweets() {
         TweetService.shared.fetchTweets { tweets in
-            print("DEBUG: Tweets are \(tweets)")
+            self.tweets = tweets
         }
     }
 
@@ -63,10 +67,12 @@ class FeedController: UICollectionViewController {
     
 }
 
+
+// MARK: - UICollectionViewDelegate/DataSource
+
 extension FeedController {
-    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return tweets.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -75,10 +81,11 @@ extension FeedController {
     }
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
+
 extension FeedController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 120)
     }
-    
 }
