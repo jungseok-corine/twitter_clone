fa15f400ffbc9e966de70050bad87d1c283c0941
jungseok-corine <nonehour@naver.com>
2024-04-21 22:05:01 +0900

    profile filters data model

---
+++ twitter_clone/ViewModel/ProfileHeaderViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileHeaderViewModel.swift
+//  twitter_clone
+//
+//  Created by 오정석 on 21/4/2024.
+//
+
+import UIKit
+
+enum ProfileFilterOptions: Int, CaseIterable {
+    case tweets
+    case replies
+    case likes
+    
+    var description: String {
+        switch self {
+        case .tweets: return "Tweets"
+        case .replies: return "Tweets & Replies"
+        case .likes: return "Likes"
+        }
+    }
+}
