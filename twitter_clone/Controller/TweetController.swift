0e8f16d13827b62891bf40362bb0b2605c8fef7b
jungseok-corine <nonehour@naver.com>
2024-05-05 00:24:48 +0900

    fetching & displaying replies

--- twitter_clone/Controller/TweetController.swift
+++ twitter_clone/Controller/TweetController.swift
@@ -15,6 +15,9 @@ class TweetController: UICollectionViewController {
     // MARK: - Properties
     
     private let tweet: Tweet
+    private var replies = [Tweet]() {
+        didSet { collectionView.reloadData() }
+    }
     
     // MARK: - Lifecycle
     
@@ -30,18 +33,24 @@ class TweetController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
-        
-        print("DEBUG: Tweet caption is \(tweet.caption)")
+        fetchReplies()
     }
     
+    // MARK: - API
+
+    func fetchReplies() {
+        TweetService.shared.fetchReplies(forTweet: tweet) { replies in
+            self.replies = replies
+        }
+    }
+    // MARK: - Helpers
+
     func configureCollectionView() {
         collectionView.backgroundColor = .white
         
         collectionView.register(TweetCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         collectionView.register(TweetHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
     }
-
-
 }
 
 // MARK: - UICollectioinViewDataSource
@@ -49,12 +58,12 @@ class TweetController: UICollectionViewController {
 extension TweetController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return replies.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TweetCell
-        
+        cell.tweet = replies[indexPath.row]
         return cell
     }
 }
