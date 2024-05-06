076e17911fd8705d31e87662e6654ff7d9f3ee81
jungseok-corine <nonehour@naver.com>
2024-05-06 15:41:00 +0900

    action sheet data model + view model

--- twitter_clone/Utils/ActionSheetLauncher.swift
+++ twitter_clone/Utils/ActionSheetLauncher.swift
@@ -16,6 +16,7 @@ class ActionSheetLauncher: NSObject {
     private let user: User
     private let tableView = UITableView()
     private var window: UIWindow?
+    private lazy var viewModel = ActionSheetViewModel(user: user)
     
     private lazy var blackView: UIView = {
        let view = UIView()
@@ -69,8 +70,6 @@ class ActionSheetLauncher: NSObject {
     // MARK: - Helpers
     
     func show() {
-        print("DEBUG: Show action sheet for user \(user.username)")
-        
         guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
         self.window = window
         
@@ -78,7 +77,7 @@ class ActionSheetLauncher: NSObject {
         blackView.frame = window.frame
         
         window.addSubview(tableView)
-        let height = CGFloat(3 * 60) + 100
+        let height = CGFloat(viewModel.options.count * 60) + 100
         tableView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
         
         UIView.animate(withDuration: 0.5) {
@@ -102,11 +101,12 @@ class ActionSheetLauncher: NSObject {
 
 extension ActionSheetLauncher: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return viewModel.options.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! ActionSheetCell
+        cell.option = viewModel.options[indexPath.row]
         return cell
     }
 }
--- twitter_clone/View/ActionSheetCell.swift
+++ twitter_clone/View/ActionSheetCell.swift
@@ -10,6 +10,10 @@ import UIKit
 class ActionSheetCell: UITableViewCell {
     // MARK: - Properties
     
+    var option: ActionSheetOptions? {
+        didSet { configure() }
+    }
+    
     private let optionImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFit
@@ -42,5 +46,11 @@ class ActionSheetCell: UITableViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    func configure() {
+        titleLabel.text = option?.description
     }
 }
