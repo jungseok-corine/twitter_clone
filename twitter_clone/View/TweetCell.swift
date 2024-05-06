97486cc1db13365e5023ea60af6300a75c2cc999
jungseok-corine <nonehour@naver.com>
2024-05-06 21:52:43 +0900

    unliking tweet

--- twitter_clone/View/TweetCell.swift
+++ twitter_clone/View/TweetCell.swift
@@ -156,6 +156,8 @@ class TweetCell: UICollectionViewCell {
         
         profileImageView.sd_setImage(with: viewModel.profileImageUrl)
         infoLabel.attributedText = viewModel.userInfoText
+        likeButton.tintColor = viewModel.likeButtonTintColor
+        likeButton.setImage(viewModel.likeButtonImage, for: .normal)
     }
 }
 
