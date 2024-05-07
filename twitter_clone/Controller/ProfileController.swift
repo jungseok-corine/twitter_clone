e4714d76151b4fba818a1963de7404f402985de2
jungseok-corine <nonehour@naver.com>
2024-05-07 15:30:36 +0900

    upload follow & reply notification

--- twitter_clone/Controller/ProfileController.swift
+++ twitter_clone/Controller/ProfileController.swift
@@ -137,6 +137,8 @@ extension ProfileController: ProfileHeaderDelegate {
             UserService.shared.followUser(uid: user.uid) { (ref, err) in
                 self.user.isFollowed = true
                 self.collectionView.reloadData()
+                
+                NotificationService.shared.uploadeNotification(type: .follow, user: self.user)
             }
         }
     }
--- twitter_clone/Controller/UploadTweetController.swift
+++ twitter_clone/Controller/UploadTweetController.swift
@@ -88,6 +88,10 @@ class UploadTweetController: UIViewController {
                 return
             }
             
+            if case .reply(let tweet) = self.config {
+                NotificationService.shared.uploadeNotification(type: .reply, tweet: tweet)
+            }
+            
             self.dismiss(animated: true, completion: nil)
         }
     }
