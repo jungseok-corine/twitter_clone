94f00acd1ca7c94c63ef44dc6475f04458dae05e
jungseok-corine <nonehour@naver.com>
2024-05-07 15:42:04 +0900

    notification table view setup

--- twitter_clone/Controller/NotificationController.swift
+++ twitter_clone/Controller/NotificationController.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 
-class NotificationController: UIViewController {
+private let reuseIdentifier = "NotificationCell"
+class NotificationController: UITableViewController {
     
     // MARK: - Properties
+    
+    private var notifications = [Notification]()
     
     // MARK: - Lifecycle
 
@@ -23,5 +26,20 @@ class NotificationController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         navigationItem.title = "Notifications"
+        
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+    }
+}
+
+extension NotificationController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
+        return cell
     }
 }
