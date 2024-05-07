6a92c60718fc6ee900c71216812a5edb07e2ef65
jungseok-corine <nonehour@naver.com>
2024-05-07 18:28:54 +0900

    fetching notifications from firebase

--- twitter_clone/API/NotificationService.swift
+++ twitter_clone/API/NotificationService.swift
@@ -6,6 +6,7 @@
 //
 
 import Firebase
+import UIKit
 
 struct NotificationService {
     static let shared = NotificationService()
@@ -24,6 +25,22 @@ struct NotificationService {
             REF_NOTIFICATIONS.child(tweet.user.uid).childByAutoId().updateChildValues(values)
         } else if let user = user {
             REF_NOTIFICATIONS.child(user.uid).childByAutoId().updateChildValues(values)
+        }
+    }
+    
+    func fetchNotifications(completion: @escaping([Notification]) -> Void) {
+        var notifications = [Notification]()
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        REF_NOTIFICATIONS.child(uid).observe(.childAdded) { snapshot in
+            guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
+            guard let uid = dictionary["uid"] as? String else { return }
+            
+            UserService.shared.fetchUser(uid: uid) { user in
+                let notification = Notification(user: user, dictionary: dictionary)
+                notifications.append(notification)
+                completion(notifications)
+            }
         }
     }
 }
--- twitter_clone/Controller/NotificationController.swift
+++ twitter_clone/Controller/NotificationController.swift
@@ -12,7 +12,9 @@ class NotificationController: UITableViewController {
     
     // MARK: - Properties
     
-    private var notifications = [Notification]()
+    private var notifications = [Notification]() {
+        didSet { tableView.reloadData() }
+    }
     
     // MARK: - Lifecycle
 
@@ -20,8 +22,17 @@ class NotificationController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        fetchNotifications()
     }
   
+    // MARK: - API
+    
+    func fetchNotifications() {
+        NotificationService.shared.fetchNotifications { notifications in
+            self.notifications = notifications
+        }
+    }
+
     // MARK: - Helpers
     func configureUI() {
         view.backgroundColor = .white
@@ -35,7 +46,7 @@ class NotificationController: UITableViewController {
 
 extension NotificationController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return notifications.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
