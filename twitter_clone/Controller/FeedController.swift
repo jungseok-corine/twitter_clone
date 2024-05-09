e9a460b275a0c6d99dca8d2b8d9f2421ba06219d
jungseok-corine <nonehour@naver.com>
2024-05-09 14:16:07 +0900

    fetching&displaying liked tweets

--- twitter_clone/Controller/FeedController.swift
+++ twitter_clone/Controller/FeedController.swift
@@ -112,7 +112,6 @@ extension FeedController {
 // MARK: - UICollectionViewDelegateFlowLayout
 
 extension FeedController: UICollectionViewDelegateFlowLayout {
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let viewModel = TweetViewModel(tweet: tweets[indexPath.row])
         let height = viewModel.size(forWidth: view.frame.width).height
