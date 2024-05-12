1fa7351b69be688b02307f2fe170df5425c6a50d
jungseok-corine <nonehour@naver.com>
2024-05-12 22:16:50 +0900

    populating cells with view model

--- twitter_clone/View/Profile/EditProfileCell.swift
+++ twitter_clone/View/Profile/EditProfileCell.swift
@@ -11,21 +11,73 @@ class EditProfileCell: UITableViewCell {
     
     // MARK: - Properties
     
+    var viewModel: EditProfileViewModel? {
+        didSet { configure() }
+    }
     
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    lazy var infoTextField: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.textAlignment = .left
+        tf.textColor = .twitterBlue
+        tf.addTarget(self, action: #selector(handleUpadteUserInfo), for: .editingDidEnd)
+        return tf
+    }()
+    
+    let bioTextView: InputTextView = {
+        let tv = InputTextView()
+        tv.font = UIFont.systemFont(ofSize: 14)
+        tv.textColor = .twitterBlue
+        tv.placeholderLabel.text = "Bio"
+        return tv
+    }()
     
     // MARK: - Lifecycle
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .systemPurple
+
+        selectionStyle = .none
+        
+        addSubview(titleLabel)
+        titleLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        titleLabel.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 16)
+        
+        addSubview(infoTextField)
+        infoTextField.anchor(top: topAnchor, left: titleLabel.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 16, paddingRight: 8)
+        
+        addSubview(bioTextView)
+        bioTextView.anchor(top: topAnchor, left: titleLabel.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 16, paddingRight: 8)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - Helpers
+    // MARK: - Selectors
 
+    @objc func handleUpadteUserInfo() {
+        
+    }
     
+    // MARK: - Helpers
     
+    func configure() {
+        guard let viewModel = viewModel else { return }
+        
+        infoTextField.isHidden = viewModel.shouldHideTextField
+        bioTextView.isHidden = viewModel.shouldHideTextView
+        
+        titleLabel.text = viewModel.titleText
+        
+        infoTextField.text = viewModel.optionValue
+        bioTextView.text = viewModel.optionValue
+    }
 }
